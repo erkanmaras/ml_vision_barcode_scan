6ad77938fc0e8c11724b53a362152b9b61b7d497
Erkan Maraş <erkanm@nebim.com.tr>
2020-09-01 12:58:20 +0300

    add ml vision api

---
+++ lib/painters.dart
@@ -0,0 +1,316 @@
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:ui' as ui;
+
+import 'package:ml_vision_barcode_scan/scanner_utils.dart';
+
+class RectangleOutlinePainter extends CustomPainter {
+  RectangleOutlinePainter({
+    @required this.animation,
+    this.strokeWidth = 3,
+  }) : super(repaint: animation);
+
+  final Animation<Rectangle> animation;
+  final double strokeWidth;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rectangle rectangle = animation.value;
+
+    final Paint paint = Paint()
+      ..strokeWidth = strokeWidth
+      ..color = rectangle.color
+      ..style = PaintingStyle.stroke;
+
+    final Offset center = size.center(Offset.zero);
+    final double halfWidth = rectangle.width / 2;
+    final double halfHeight = rectangle.height / 2;
+
+    final Rect rect = Rect.fromLTRB(
+      center.dx - halfWidth,
+      center.dy - halfHeight,
+      center.dx + halfWidth,
+      center.dy + halfHeight,
+    );
+
+    canvas.drawRect(rect, paint);
+  }
+
+  @override
+  bool shouldRepaint(RectangleOutlinePainter oldDelegate) => false;
+}
+
+class RectangleTracePainter extends CustomPainter {
+  RectangleTracePainter({
+    @required this.animation,
+    @required this.rectangle,
+    this.strokeWidth = 3,
+  }) : super(repaint: animation);
+
+  final Animation<double> animation;
+  final Rectangle rectangle;
+  final double strokeWidth;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double value = animation.value;
+
+    final Offset center = size.center(Offset.zero);
+    final double halfWidth = rectangle.width / 2;
+    final double halfHeight = rectangle.height / 2;
+
+    final Rect rect = Rect.fromLTRB(
+      center.dx - halfWidth,
+      center.dy - halfHeight,
+      center.dx + halfWidth,
+      center.dy + halfHeight,
+    );
+
+    final Paint paint = Paint()
+      ..strokeWidth = strokeWidth
+      ..color = rectangle.color;
+
+    final double halfStrokeWidth = strokeWidth / 2;
+
+    final double heightProportion = (halfStrokeWidth + rect.height) * value;
+    final double widthProportion = (halfStrokeWidth + rect.width) * value;
+
+    canvas.drawLine(
+      Offset(rect.right, rect.bottom + halfStrokeWidth),
+      Offset(rect.right, rect.bottom - heightProportion),
+      paint,
+    );
+
+    canvas.drawLine(
+      Offset(rect.right + halfStrokeWidth, rect.bottom),
+      Offset(rect.right - widthProportion, rect.bottom),
+      paint,
+    );
+
+    canvas.drawLine(
+      Offset(rect.left, rect.top - halfStrokeWidth),
+      Offset(rect.left, rect.top + heightProportion),
+      paint,
+    );
+
+    canvas.drawLine(
+      Offset(rect.left - halfStrokeWidth, rect.top),
+      Offset(rect.left + widthProportion, rect.top),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(RectangleTracePainter oldDelegate) => false;
+}
+
+
+class BarcodeDetectorPainter extends CustomPainter {
+  BarcodeDetectorPainter(this.absoluteImageSize, this.barcodeLocations);
+
+  final Size absoluteImageSize;
+  final List<Barcode> barcodeLocations;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    Rect scaleRect(Barcode barcode) {
+      return Rect.fromLTRB(
+        barcode.boundingBox.left * scaleX,
+        barcode.boundingBox.top * scaleY,
+        barcode.boundingBox.right * scaleX,
+        barcode.boundingBox.bottom * scaleY,
+      );
+    }
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    for (Barcode barcode in barcodeLocations) {
+      paint.color = Colors.green;
+      canvas.drawRect(scaleRect(barcode), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(BarcodeDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.barcodeLocations != barcodeLocations;
+  }
+}
+
+class FaceDetectorPainter extends CustomPainter {
+  FaceDetectorPainter(this.absoluteImageSize, this.faces);
+
+  final Size absoluteImageSize;
+  final List<Face> faces;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0
+      ..color = Colors.red;
+
+    for (Face face in faces) {
+      canvas.drawRect(
+        Rect.fromLTRB(
+          face.boundingBox.left * scaleX,
+          face.boundingBox.top * scaleY,
+          face.boundingBox.right * scaleX,
+          face.boundingBox.bottom * scaleY,
+        ),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.faces != faces;
+  }
+}
+
+class LabelDetectorPainter extends CustomPainter {
+  LabelDetectorPainter(this.absoluteImageSize, this.labels);
+
+  final Size absoluteImageSize;
+  final List<ImageLabel> labels;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final ui.ParagraphBuilder builder = ui.ParagraphBuilder(
+      ui.ParagraphStyle(
+          textAlign: TextAlign.left,
+          fontSize: 23.0,
+          textDirection: TextDirection.ltr),
+    );
+
+    builder.pushStyle(ui.TextStyle(color: Colors.green));
+    for (ImageLabel label in labels) {
+      builder.addText('Label: ${label.text}, '
+          'Confidence: ${label.confidence.toStringAsFixed(2)}\n');
+    }
+    builder.pop();
+
+    canvas.drawParagraph(
+      builder.build()
+        ..layout(ui.ParagraphConstraints(
+          width: size.width,
+        )),
+      const Offset(0.0, 0.0),
+    );
+  }
+
+  @override
+  bool shouldRepaint(LabelDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.labels != labels;
+  }
+}
+
+// Paints rectangles around all the text in the image.
+class TextDetectorPainter extends CustomPainter {
+  TextDetectorPainter(this.absoluteImageSize, this.visionText);
+
+  final Size absoluteImageSize;
+  final VisionText visionText;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    Rect scaleRect(TextContainer container) {
+      return Rect.fromLTRB(
+        container.boundingBox.left * scaleX,
+        container.boundingBox.top * scaleY,
+        container.boundingBox.right * scaleX,
+        container.boundingBox.bottom * scaleY,
+      );
+    }
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement element in line.elements) {
+          paint.color = Colors.green;
+          canvas.drawRect(scaleRect(element), paint);
+        }
+
+        paint.color = Colors.yellow;
+        canvas.drawRect(scaleRect(line), paint);
+      }
+
+      paint.color = Colors.red;
+      canvas.drawRect(scaleRect(block), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(TextDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.visionText != visionText;
+  }
+}
+
+// Paints rectangles around all the text in the document image.
+class DocumentTextDetectorPainter extends CustomPainter {
+  DocumentTextDetectorPainter(this.absoluteImageSize, this.visionDocumentText);
+
+  final Size absoluteImageSize;
+  final VisionDocumentText visionDocumentText;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    Rect scaleRect(DocumentTextContainer container) {
+      return Rect.fromLTRB(
+        container.boundingBox.left * scaleX,
+        container.boundingBox.top * scaleY,
+        container.boundingBox.right * scaleX,
+        container.boundingBox.bottom * scaleY,
+      );
+    }
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    for (DocumentTextBlock block in visionDocumentText.blocks) {
+      for (DocumentTextParagraph paragraph in block.paragraphs) {
+        for (DocumentTextWord word in paragraph.words) {
+          for (DocumentTextSymbol symbol in word.symbols) {
+            paint.color = Colors.green;
+            canvas.drawRect(scaleRect(symbol), paint);
+          }
+          paint.color = Colors.yellow;
+          canvas.drawRect(scaleRect(word), paint);
+        }
+        paint.color = Colors.red;
+        canvas.drawRect(scaleRect(paragraph), paint);
+      }
+      paint.color = Colors.blue;
+      canvas.drawRect(scaleRect(block), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(DocumentTextDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.visionDocumentText != visionDocumentText;
+  }
+}
